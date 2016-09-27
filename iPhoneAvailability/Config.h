fd4c6b923f0827626f52f763896836cc79845b4f
Wutian <naituw@gmail.com>
2016-09-27 12:06:12 +0800

    Update Config.h

--- iPhoneAvailability/Config.h
+++ iPhoneAvailability/Config.h
@@ -12,7 +12,7 @@
 #define StoresURL @"https://reserve.cdn-apple.com/CN/zh_CN/reserve/iPhone/stores.json"
 #define AvailabilityURL @"https://reserve.cdn-apple.com/CN/zh_CN/reserve/iPhone/availability.json"
 
-// TargetModel: 例如 @"MN8L2ZP/A" 代表 iPhone 7 128GB 黑色 https://reserve.cdn-apple.com/CN/zh_CN/reserve/iPhone/availability.json
+// TargetModel: 例如 @"MNGX2CH/A" 代表 iPhone 7 128GB 黑色 https://reserve.cdn-apple.com/CN/zh_CN/reserve/iPhone/availability.json
 #define TargetModel @""
 
 // TargetModel: 例如 @"北京"，请从 Apple 的接口中找到合适的名称 https://reserve.cdn-apple.com/CN/zh_CN/reserve/iPhone/stores.json
